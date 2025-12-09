e8c7ca584eae5a3944daf3a66acd832f25b8e8a4
Manojkumar-mk41 <r.manojkumar7878@gmail.com>
2025-12-09 06:40:26 +0530

    posts update, delete functions added

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -39,4 +39,52 @@ const getPosts = async (req, res) => {
   }
 };
 
-export { createPost, getPosts };
+const updatePost = async (req, res) => {
+  try {
+    if (Object.keys(req.body) < 1) {
+      return res.status(400).json({
+        message: "No data provided to update",
+      });
+    }
+
+    const post = await Post.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.status(200).json({
+      message: "Post updated successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findByIdAndDelete(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        message: "Post not found",
+      });
+    }
+    res.status(200).json({
+      message: "Post deleted successfully",
+      post,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server error",
+      error,
+    });
+  }
+};
+
+export { createPost, getPosts, updatePost, deletePost };
